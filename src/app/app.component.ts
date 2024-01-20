87a9d51620fd287f5785879d758b9d096111730e
IviKeiko <jooly.klee@gmail.com>
2024-01-20 23:45:33 +0100

    data model, ngIf, binding data to properties and attr, binding events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items: WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Take a Walk', true),
+    new WishItem('Play Boardgame'),
+  ];
   title = 'wishlist';
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
